11a7cbfb4d0d3109f1db9c3098d1fe871c9c4bbc
brosell1 <brosell@live.co.uk>
2017-12-05 16:57:33 +0000

    added modal popup and a crappy image upload function that doesn't work

--- my/src/components/Form.js
+++ my/src/components/Form.js
@@ -3,11 +3,11 @@ import React, {Component} from 'react';
 class Form extends Component {
   render() {
     return(
-      <div>
+      <form onSubmit={this.props.onClick} encType="multipart/form-data">
         <input onChange={this.props.onChange} value={this.props.tweet} name="tweet"/><br/>
         <input onChange={this.props.onChange} value={this.props.media} name="media" type="file" /><br/>
-        <button onClick={this.props.onClick} id="submit">submit</button>
-      </div>
+        <input type="submit" />
+      </form>
     );
   };
 };
