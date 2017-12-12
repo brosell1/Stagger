c579ff8097c66ca386ab77616e84eefcbdaaa78f
brosell1 <brosell@live.co.uk>
2017-12-12 09:43:44 +0000

    added a bit of react stuff

--- my-app/src/views/Post.js
+++ my-app/src/views/Post.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+
 import Form from '../components/Form.js';
 import Popup from '../components/Popup.js';
 
 const Post = (props) => {
   return(<div>
-    <Form onChange={props.onChange} onClick={props.onClick} tweet={props.tweet} media={props.media} />
+  <Card style={{
+    margin: '2em',
+    width: '95vw',
+    padding: '2em'
+  }}>
+    <Form onChange={props.onChange} onClick={props.onClick} tweet={props.tweet} media={props.media} tags={props.tags}/>
     <Popup open={props.open} closePopup={props.closePopup} statusOk={props.statusOk}/>
-  </div>)
+  </Card>
+</div>)
 }
 
 export default Post
